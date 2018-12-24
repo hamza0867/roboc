bbc5c88fcfa088d73a765323bb3ba18d49b7b080
hamza0867 <hamza0867@gmail.com>
2018-12-24 20:14:16 +0100

    Upgrade Server to communicate with multiple clients

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-TODO List
----------
+## TODO List
 
-- [x] adding walls
-- [x] adding doors
-- [x] upgrading the simple server end of the game
-- [x] upgrading the clent end of the game
-- [ ] upgrading the server to communicate with multiple clients
+-   [x] adding walls
+-   [x] adding doors
+-   [x] upgrading the simple server end of the game
+-   [x] upgrading the clent end of the game
+-   [x] upgrading the server to communicate with multiple clients
+-   [x] upgrading the client to listen to player and server on different threads
--- roboc_client.py
+++ roboc_client.py
@@ -1,5 +1,19 @@
+import enum
+import pickle
 import re
 import socket
+
+import my_message
+
+movement_expression = r"^[nesoNSEO][1-9]?[0-9]*$"
+building_expression = r"^[mpMP][nseoNSEO]$"
+quit_expression = r"^[qQ]$"
+commande_expression = "|".join(
+    [movement_expression, building_expression, quit_expression])
+
+
+class ClientCommand(enum.Enum):
+    STOP_CLIENT = enum.auto
 
 
 def start_client():
@@ -8,25 +22,35 @@ def start_client():
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        movement_expression = r"^[nesoNSEO][1-9]?[0-9]*$"
-        building_expression = r"^[mpMP][nseoNSEO]$"
-        quit_expression = r"^[qQ]$"
-        commande_expression = "|".join(
-            [movement_expression, building_expression, quit_expression])
 
         while True:
-            maze = s.recv(1024).decode()
-            print(maze)
-            if maze == "Game Over":
-                return
-            commande = input("\n> ")
+            data = s.recv(1024)
+            message = pickle.loads(data)
 
-            while not re.match(commande_expression, commande):
-                print(
-                    "\nThe commande you typed does not exist, please type a valid\
-                      commande")
-                commande = input("\n> ")
-            s.send(commande.encode())
+            if handle_message(message, s) is ClientCommand.STOP_CLIENT:
+                return
+
+
+def handle_message(message, socket):
+    if message.message_type in {
+            my_message.MessageType.INFO,
+            my_message.MessageType.MAZE,
+    }:
+        print(message.string_message)
+    elif message.message_type is my_message.MessageType.PLAY:
+        print("PLAY message made it")
+        commande = input("\n> ")
+
+        while not re.match(commande_expression, commande):
+            print(
+                "\nThe commande you typed does not exist, please type a valid\
+                  commande")
+            commande = input("\n> ")
+        socket.send(commande.encode())
+    elif message.message_type is my_message.MessageType.GAME_OVER:
+        print("Game Over. See you next time !!")
+
+        return ClientCommand.STOP_CLIENT
 
 
 if __name__ == "__main__":
